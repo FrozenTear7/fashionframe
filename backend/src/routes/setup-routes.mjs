13fd9bd685e6f5a573a7aaceea072d11344871f6
Paweł Mendroch <pawelmendroch7@gmail.com>
2020-02-23 19:18:35 +0100

    Add update, private routing and some query fixes

--- backend/src/routes/setup-routes.mjs
+++ backend/src/routes/setup-routes.mjs
@@ -11,7 +11,7 @@ router.get("/", async (req, res) => {
 
   try {
     let setupsQueryString =
-      "SELECT u.username, s.id, s.name, s.screenshot, s.frame, s.created_at, (SELECT COUNT(*) FROM setups_users WHERE user_id = u.id AND setup_id = s.id) AS liked FROM setups s\n" +
+      "SELECT u.username, s.id, s.name, s.screenshot, s.frame, s.created_at, (SELECT COUNT(*) FROM setups_users WHERE setup_id = s.id) AS liked FROM setups s\n" +
       "JOIN users u ON u.id = s.user_id";
 
     if (req.query.frame && frames.frames.includes(req.query.frame))
@@ -51,7 +51,7 @@ router.get("/:id", async (req, res) => {
 
   try {
     const setup = await client.query(
-      "SELECT u.username, s.*, (SELECT COUNT(*) FROM setups_users WHERE user_id = u.id AND setup_id = $2) AS liked,\n" +
+      "SELECT u.id AS userid, u.username, s.*, (SELECT COUNT(*) FROM setups_users WHERE setup_id = $2) AS liked,\n" +
         "EXISTS(SELECT 1 FROM setups_users WHERE user_id=$1 AND setup_id = $2) AS likedbyyou FROM setups s JOIN users u ON u.id = s.user_id WHERE s.id = $2",
       [req.user.id, req.params.id]
     );
@@ -244,6 +244,158 @@ router.post(
       console.log(err);
       res.status(400).send({
         message: "Could not create setup"
+      });
+    } finally {
+      client.release();
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  connectEnsureLogin.ensureLoggedIn("http://localhost:3000/signin"),
+  async (req, res) => {
+    const client = await pool.connect();
+
+    try {
+      const checkSetup = await client.query(
+        "SELECT user_id FROM setups WHERE id = $1",
+        [req.params.id]
+      );
+
+      if (checkSetup.rows[0].user_id !== req.user.id)
+        res.status(403).send({
+          message: "Setup editing not allowed"
+        });
+
+      await client.query("BEGIN");
+
+      const setup = req.body.setup;
+      const setupColorScheme = setup.colorScheme;
+      const attachments = setup.attachments;
+      const attachmentsColorScheme = attachments.colorScheme;
+      const syandana = setup.syandana;
+      const syandanaColorScheme = syandana.colorScheme;
+
+      const updateSetup = await client.query(
+        "UPDATE setups SET name = $1, frame = $2, description = $3, screenshot = $4, helmet = $5, skin = $6\n" +
+          "WHERE id = $7 RETURNING id, attachment_id, syandana_id, color_scheme_id",
+        [
+          setup.name,
+          setup.frame,
+          setup.description,
+          setup.screenshot,
+          setup.helmet,
+          setup.skin,
+          req.params.id
+        ]
+      );
+
+      const updateAttachments = await client.query(
+        "UPDATE attachments SET chest = $1, left_arm = $2, right_arm = $3, left_leg = $4, right_leg = $5, ephemera = $6\n" +
+          "WHERE id = $7 RETURNING id",
+        [
+          attachments.chest,
+          attachments.leftArm,
+          attachments.rightArm,
+          attachments.leftLeg,
+          attachments.rightLeg,
+          attachments.ephemera,
+          updateSetup.rows[0].attachment_id
+        ]
+      );
+
+      const updateSyandana = await client.query(
+        "UPDATE syandanas SET name = $1 WHERE id = $2 RETURNING id",
+        [syandana.name, updateSetup.rows[0].syandana_id]
+      );
+
+      const updateSetupColorScheme = await client.query(
+        'UPDATE color_schemes SET "primary" = $1, "secondary" = $2, tertiary = $3, accents = $4, emmissive1 = $5, emmissive2 = $6, energy1 = $7, energy2 = $8\n' +
+          "WHERE id = $9 RETURNING id",
+        [
+          setupColorScheme.primary,
+          setupColorScheme.secondary,
+          setupColorScheme.tertiary,
+          setupColorScheme.accents,
+          setupColorScheme.emmissive1,
+          setupColorScheme.emmissive2,
+          setupColorScheme.energy1,
+          setupColorScheme.energy2,
+          updateSetup.rows[0].color_scheme_id
+        ]
+      );
+
+      const updateAttachmentsColorScheme = await client.query(
+        'UPDATE color_schemes SET "primary" = $1, "secondary" = $2, tertiary = $3, accents = $4, emmissive1 = $5, emmissive2 = $6, energy1 = $7, energy2 = $8\n' +
+          "WHERE id = $9 RETURNING id",
+        [
+          attachmentsColorScheme.primary,
+          attachmentsColorScheme.secondary,
+          attachmentsColorScheme.tertiary,
+          attachmentsColorScheme.accents,
+          attachmentsColorScheme.emmissive1,
+          attachmentsColorScheme.emmissive2,
+          attachmentsColorScheme.energy1,
+          attachmentsColorScheme.energy2,
+          updateAttachments.rows[0].color_scheme_id
+        ]
+      );
+
+      const updateSyandanaColorScheme = await client.query(
+        'UPDATE color_schemes SET "primary" = $1, "secondary" = $2, tertiary = $3, accents = $4, emmissive1 = $5, emmissive2 = $6, energy1 = $7, energy2 = $8\n' +
+          "WHERE id = $9 RETURNING id",
+        [
+          syandanaColorScheme.primary,
+          syandanaColorScheme.secondary,
+          syandanaColorScheme.tertiary,
+          syandanaColorScheme.accents,
+          syandanaColorScheme.emmissive1,
+          syandanaColorScheme.emmissive2,
+          syandanaColorScheme.energy1,
+          syandanaColorScheme.energy2,
+          updateSyandana.rows[0].color_scheme_id
+        ]
+      );
+
+      await client.query("COMMIT");
+      res.send({ setupId: updateSetup.rows[0].id });
+    } catch (err) {
+      await client.query("ROLLBACK");
+      console.log(err);
+      res.status(400).send({
+        message: "Could not create setup"
+      });
+    } finally {
+      client.release();
+    }
+  }
+);
+
+router.delete(
+  "/:id",
+  connectEnsureLogin.ensureLoggedIn("http://localhost:3000/signin"),
+  async (req, res) => {
+    const client = await pool.connect();
+
+    try {
+      await client.query("DELETE FROM setups WHERE id = $1 AND user_id = $2", [
+        req.params.id,
+        req.user.id
+      ]);
+
+      await client.query(
+        "DELETE FROM setups_users WHERE setup_id = $1 AND user_id = $2",
+        [req.params.id, req.user.id]
+      );
+
+      await client.query("COMMIT");
+      res.sendStatus(200);
+    } catch (err) {
+      await client.query("ROLLBACK");
+      console.log(err);
+      res.status(400).send({
+        message: "Could not delete setup"
       });
     } finally {
       client.release();
